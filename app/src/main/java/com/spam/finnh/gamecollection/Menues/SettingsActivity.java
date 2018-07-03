391a4b0626aa33cac6ec2bbc3c4c7ed19e836879
Mai65 <finnh.spam@outlook.de>
2018-07-03 10:49:55 +0200

    fixed settings

--- app/src/main/java/com/spam/finnh/gamecollection/Menues/SettingsActivity.java
+++ app/src/main/java/com/spam/finnh/gamecollection/Menues/SettingsActivity.java
@@ -12,10 +12,10 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
-import android.support.v7.app.ActionBar;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
@@ -148,7 +148,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      */
     @Override
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void onBuildHeaders(List<PreferenceActivity.Header> target) {
+    public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
