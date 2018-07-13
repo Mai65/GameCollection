7c2ded25d0d91f8e3bb2e45e4a3d7ceec22dfa6d
DanielK79 <dani.kerger@gmail.com>
2018-07-13 19:36:04 +0200

    added checkTurn and if boat is on box

--- app/src/main/java/com/spam/finnh/gamecollection/Battleship/Battleship_playarea.java
+++ app/src/main/java/com/spam/finnh/gamecollection/Battleship/Battleship_playarea.java
@@ -18,11 +18,22 @@ public class Battleship_playarea extends AppCompatActivity {
 
     ConstraintLayout Layout;
 
-    //Array where boats are on the filed for reload
+    //How is playing (true = A --- false = b)
     public boolean turn;
+
+    //Array where boats are on the filed for reload
     public int field_a[][];
     public int field_b[][];
     public int round;
+
+    //Explinaition of the int's in the fields:
+    //0 = No boat
+    //1 = boat
+    //2 = destroyed boat
+
+    int noBoat = 0;
+    int boat = 1;
+    int destroyedBoat = 2;
 
     //Coordinates where you touch on the Screen
     float xLocation = 0;
@@ -43,6 +54,7 @@ public class Battleship_playarea extends AppCompatActivity {
     public Battleship_playarea() {
         turn = false;
         round = 0;
+
 
         //Field of player A (10/10)
         field_a = new int[10][10];
@@ -87,8 +99,8 @@ public class Battleship_playarea extends AppCompatActivity {
             setBoats(null);
 
             //Toast out the Coordinates
-            String text = +XcalculatedPosition + "und" + YcalculatedPosition;
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            //String text = +XcalculatedPosition + "und" + YcalculatedPosition;
+            //Toast.makeText(this, text, Toast.LENGTH_LONG).show();
         }
         return super.onTouchEvent(event);
     }
@@ -112,41 +124,70 @@ public class Battleship_playarea extends AppCompatActivity {
 
     public void ImageSize() {
         //set the boat size in relation to the screen size
-        width = (float) ((maxX*0.19)-(maxX*0.1));
-        height = (float) ((maxY*0.305)- (maxY*0.25));
+        width = (float) ((maxX * 0.19) - (maxX * 0.1));
+        height = (float) ((maxY * 0.305) - (maxY * 0.25));
     }
 
     public void setBoats(View view) {
 
         if (XcalculatedPosition > 0.1 && XcalculatedPosition < 0.19 && YcalculatedPosition > 0.25 && YcalculatedPosition < 0.305) {
+            checkTurn();
+        }
+            else if (XcalculatedPosition > 0.19 && XcalculatedPosition < 0.27 && YcalculatedPosition > 0.25 && YcalculatedPosition < 0.305) {
+                ImageView imageViewA1 = new ImageView(this);
+                imageViewA1.setImageResource(R.drawable.battleship_red);
 
-            //Create a new boat and get the colour of the boat
-            ImageView imageViewA1 = new ImageView(this);
-            imageViewA1.setImageResource(R.drawable.battleship_blue);
+                imageViewA1.setX(maxX * XcalculatedPosition);
+                imageViewA1.setY(maxY * YcalculatedPosition);
+                imageViewA1.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT));
 
-            //Set the coordinates of the boat (top-left corner has to be used)
-            imageViewA1.setX((float) (0.11*maxX));
-            imageViewA1.setY((float) (0.26*maxY));
+                Layout.addView(imageViewA1);
+                setContentView(Layout);
+            }
+        }
 
-            //Set the width and height of the boat with the variables of ImageSize()
-            imageViewA1.setLayoutParams(new RelativeLayout.LayoutParams((int) width,
-                    (int) height));
+    public void createBoat() {
+        //Create a new boat and get the colour of the boat
+        ImageView imageViewA1 = new ImageView(this);
+        imageViewA1.setImageResource(R.drawable.battleship_blue);
 
-            //Add the ImageView to the Layout
-            Layout.addView(imageViewA1);
-            setContentView(Layout);
+        //Set the coordinates of the boat (top-left corner has to be used)
+        imageViewA1.setX((float) (0.11 * maxX));
+        imageViewA1.setY((float) (0.26 * maxY));
 
-        } else if(XcalculatedPosition > 0.19 && XcalculatedPosition < 0.27 && YcalculatedPosition > 0.25 && YcalculatedPosition < 0.305){
-            ImageView imageViewA1 = new ImageView(this);
-            imageViewA1.setImageResource(R.drawable.battleship_red);
+        //Set the width and height of the boat with the variables of ImageSize()
+        imageViewA1.setLayoutParams(new RelativeLayout.LayoutParams((int) width,
+                (int) height));
 
-            imageViewA1.setX(maxX*XcalculatedPosition);
-            imageViewA1.setY(maxY*YcalculatedPosition);
-            imageViewA1.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT));
+        //Add the ImageView to the Layout
+        Layout.addView(imageViewA1);
+        setContentView(Layout);
+    }
 
-            Layout.addView(imageViewA1);
-            setContentView(Layout);
+    public void checkTurn() {
+        //Check who's turn is
+        if (turn) {
+            //Check if there is already something on the box
+            if (field_a[0][0] != noBoat) {
+                //Generate Toast with Error message "Occupied Field"
+                String fieldOccupied = getString(R.string.field_occupied);
+                Toast.makeText(this, fieldOccupied, Toast.LENGTH_LONG).show();
+                //Let the player choose another box
+            } else {
+                createBoat();
+                field_a[0][0] = 1;
+            }
+        } else {
+            if (field_b[0][0] != noBoat) {
+                //Generate Toast with Error message "Occupied Field"
+                String fieldOccupied = getString(R.string.field_occupied);
+                Toast.makeText(this, fieldOccupied, Toast.LENGTH_LONG).show();
+                //Let the player choose another box
+            } else {
+                createBoat();
+                field_b[0][0] = 1;
+            }
         }
     }
 }
